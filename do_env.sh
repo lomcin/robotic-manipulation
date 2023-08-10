1858e892892474b6fb84ecd889309645cdd28971
Lucas Oliveira Maggi <lucas.maggi@usp.br>
2023-08-10 10:26:10 -0300

    [ADDED] mujoco, pygame in do_env.sh script.

---
+++ do_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -d "venv" ]; then
+    source venv/bin/activate
+else
+    python3 -m pip install virtualenv
+    virtualenv venv
+    source venv/bin/activate
+    python3 -m pip install --upgrade pip
+    python3 -m pip install pygame mujoco
+fi
