18f2abc9c6210ee6ecf5496312929f302ddf1a09
Lucas Oliveira Maggi <lucasomaggi@gmail.com>
2023-12-13 16:44:09 -0300

    [ADDED] pytorch version and stable-baselines3 extras for CUDA support.

--- do_env.sh
+++ do_env.sh
@@ -7,5 +7,7 @@ else
     virtualenv venv
     source venv/bin/activate
     python3 -m pip install --upgrade pip
-    python3 -m pip install pygame scipy swig "gymnasium[all]" mujoco stable-baselines3
+    # Install torch with cuda
+    python3 -m pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu121
+    python3 -m pip install pygame scipy swig "gymnasium[all]" mujoco stable-baselines3[extra]
 fi
